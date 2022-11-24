79f1002b03b828abfce3610f838930fb5394449f
Mathanstunnerz <118900739+Mathanstunnerz@users.noreply.github.com>
2022-11-24 23:00:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var resume = {
+
+"name" :"mathan_kumar",
+"age"  :"20",
+"about_me":"i_am_professsional_editor&developer.",
+"languages" :"tamil,english",
+"education": "10th,diploma(mech),developer",
+"experience": "0",
+"hobbies":"flim_makeing,photography,writing,music",
+"profession_skills" : "photoshop,premirepro,illustrator,aftereffect,mocapro,davince_resolve,avid"
+
+}
+console.log(resume);
+
+
+
+
